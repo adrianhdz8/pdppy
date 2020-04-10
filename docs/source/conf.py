73581a2cd09c7986b1bdf01e76e33bc4138efc8b
adrianhdz8 <63477654+adrianhdz8@users.noreply.github.com>
2020-04-10 19:23:19 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,6 +41,10 @@ extensions = [
 ]
 
 
+# The master toctree document.
+master_doc = "index"
+
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
