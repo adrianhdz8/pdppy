1e2eb4c872876101a905106ce223e9eb91648c52
adrianhdz8 <63477654+adrianhdz8@users.noreply.github.com>
2020-04-14 18:08:22 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 
 __author__ = 'Adrian Hernandez <ah695@cornell.edu>'
 
-__all__ = ['instances', 'algorithms', 'plot', 'helper']
-
-
+from pdppy import instances
+from pdppy import algorithms
+from pdppy import plot
+from pdppy import helper
